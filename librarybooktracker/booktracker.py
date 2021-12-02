44cab1182d4812811fdd63d78fb91b38aea88ca2
CallumBlair <s5330191@bournemouth.ac.uk>
2021-12-02 10:34:38 +0000

    Update booktracker.py

--- librarybooktracker/booktracker.py
+++ librarybooktracker/booktracker.py
@@ -10,11 +10,11 @@ from SQL.sqlclass import *
 database = mbdb()
 
 
-##app = Flask(__name__)
-##
-##@app.route("/")
-##def index():
-##    return render_template("index.html")
-##
-##if __name__ == '__main__':
-##    app.run()
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run()
