27c85a4bc929f2d4db5c8fc52044f13ce771a6ee
s5305266 <s5305266@bournemouth.ac.uk>
2021-12-15 14:48:05 +0000

    Changed file names, updated JS

--- librarybooktracker/JS/BookTracker.js
+++ librarybooktracker/JS/BookTracker.js
@@ -1,13 +1,36 @@
 //JavaScript for HTML in templates folder
 
-function api(info) {
-}
-
 document.getElementById("LogInButton").addEventListener("click", LogInFunc)
 
 function LogInFunc(){
 	let username = document.getElementById("Username").value;
 	let password = document.getElementById("Password").value;
 	let info = "/api/login?username=" + username + "&password=" + password;
-	api(info); //starts api function to send information to server
+	apiLogIn(info); //starts api function to send information to flask
+	
+	console.log(username + "" + "" + password); //For testing purposes, delete later
+}
+
+function apiLogIn(info) { //used to send information to flask
+	let flask = new XMLHttpRequest();
+	
+	flask.open("GET", info, true);
+	flask.send();
+}
+
+document.getElementById("MemberButton").addEventListener("click", NewMemFunc)
+
+function NewMemFunc(){
+	let name = document.getElementById("Member").value;
+	let info = "/api/newmember?name=" + name;
+	apiNewMem(info); //starts a function to send information to flask
+	
+	console.log(name); //For testing purposes, delete later
+}
+
+function apiNewMem(info) {
+	let flask = new XMLHttpRequest();
+	
+	flask.open("GET", info, true);
+	flask.send();
 }
