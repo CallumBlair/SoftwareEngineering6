c55e42c995baf39af3b7d15773e02b20c004e3c3
aidanpentarakis <92801412+aidanpentarakis@users.noreply.github.com>
2021-12-16 12:41:48 +0000

    Added a form for the log in html. Added a login route in the main python file
    Added a form for the log in html. Added a login route in the main python file

--- librarybooktracker/booktracker.py
+++ librarybooktracker/booktracker.py
@@ -10,12 +10,32 @@ from SQL.sqlclass import *
 
 #database = mydb()
 
-
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/login", methods=['GET','POST'])
+def login():
+     #Check if "username" and "password" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        # Create variables for easy access
+        username = request.form['Username']
+        password = request.form['Password']
+        mycursor = self.mydb.cursor()
+        mycursor.execute('SELECT * FROM staff_tbl WHERE staff_name = %s AND staff_pw = %s', (username, password,))
+        # Fetch one record and return result
+        account = mycursor.fetchone()
+        # If account exists in accounts table in out database
+        if account:
+            # Create session data, we can access this data in other routes
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            # Redirect to home page
+            return 'Logged in successfully!'
+    return render_template("log-in.html")
 
 @app.route("/datasearch")
 def dataSearchView():
